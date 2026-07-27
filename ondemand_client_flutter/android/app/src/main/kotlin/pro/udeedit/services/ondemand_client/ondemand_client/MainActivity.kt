e6e94971fcef7727ee96420334900dbd23a43420
Sargius <Dr.Sargius@gmail.com>
2026-07-27 23:40:42 +0400

    Initialize OnDemand monorepo with Flutter client and master apps
    - Create ondemand_client_flutter with BLoC architecture
    - Create ondemand_master_flutter with BLoC architecture
    - Both apps configured with organization: pro.udeedit.services
    - Material 3 UI ready for development
    - Mocked data structure prepared for next phase

---
+++ ondemand_client_flutter/android/app/src/main/kotlin/pro/udeedit/services/ondemand_client/ondemand_client/MainActivity.kt
@@ -0,0 +1,5 @@
+package pro.udeedit.services.ondemand_client.ondemand_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ondemand_master_flutter/android/app/src/main/kotlin/pro/udeedit/services/ondemand_master/ondemand_master/MainActivity.kt
@@ -0,0 +1,5 @@
+package pro.udeedit.services.ondemand_master.ondemand_master
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
